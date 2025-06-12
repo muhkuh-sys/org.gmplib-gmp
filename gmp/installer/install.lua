ffb4e88cb5c8ed72604ff4a433c2605e475a53d5
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2025-06-12 17:02:53 +0200

    Add the pkg-config to the artifact.

--- gmp/installer/install.lua
+++ gmp/installer/install.lua
@@ -5,9 +5,10 @@ local tResult
 if strDistId == '@JONCHKI_PLATFORM_DIST_ID@' and
 strDistVersion == '@JONCHKI_PLATFORM_DIST_VERSION@' and
 strCpuArch == '@JONCHKI_PLATFORM_CPU_ARCH@' then
-  t:install_dev('include',    '${install_dev_include}/')
-  t:install_dev('lib',      '${install_dev_lib}/')
-  tResult = true 
+  t:install_dev('include',   '${install_dev_include}/')
+  t:install_dev('lib',       '${install_dev_lib}/')
+  t:install_dev('pkgconfig', '${install_dev}/pkgconfig/')
+  tResult = true
 end
 
-return tResult 
+return tResult
